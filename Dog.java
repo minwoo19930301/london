665f7d8dcc2a969e623372aa3da3b1a796d98db8
김민우 <49029456+minwoo19930301@users.noreply.github.com>
2025-04-10 22:34:41 +0900

    add Dog

---
+++ Dog.java
@@ -0,0 +1,30 @@
+public class Dog {
+    int age;
+    String name;
+    Dog enemy;
+    int hp;
+    int strength;
+
+
+    Dog(){
+        this.age = 1;
+        this.hp = 30;
+        this.strength = 10;
+        System.out.println("아 ~~ 태어났다~~~");
+    }
+
+    Dog(int age, int hp, int strength){
+        this.age = age;
+        this.hp = hp;
+        this.strength = strength;
+    }
+
+    void bark() {
+        System.out.println("my name is " + this.name + " ruff ruff !");
+    }
+
+    int getAge(int age){
+        return this.age + age;
+    }
+
+}
